e6c5de6dd86c8f2811e1e8fafa7a37ca1d6605de
Mohammad Sohail Akhtar <akhtarsohail4429@gmail.com>
2026-01-16 21:40:04 +0545

    Handle error

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,17 +1,58 @@
 'use client'
 
-import { Button, TextField } from '@radix-ui/themes';
+import { Button, Callout, TextField } from '@radix-ui/themes';
 import SimpleMDE from "react-simplemde-editor";
+import axios from "axios"
+import { useForm, Controller, SubmitHandler } from "react-hook-form"
 import "easymde/dist/easymde.min.css";
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+
+
+
+interface IssueForm {
+    title: string,
+    description: string
+}
 
 
 const NewIssuePage = () => {
+
+    const [errors, setErrors] = useState('')
+
+    const navigate = useRouter()
+
+   const {register, control, handleSubmit} = useForm<IssueForm>()
+//    console.log(control)
+
+   const onSubmit: SubmitHandler<IssueForm> = async (data) => {
+    try {
+        await axios.post('/api/issues', data)
+        toast.success('Issue created successfully!')
+        navigate.push('/issues')
+    } catch (error:any) {
+        setErrors("Please fill the title and description")
+    }   
+   }
+
   return (
-    <div className='max-w-xl space-y-3'>
-        <TextField.Root placeholder='Title' />
-        <SimpleMDE placeholder='Description'/>
+    <div className='max-w-xl '>
+        {errors && (
+            <Callout.Root color='red' className='mb-5'>
+                <Callout.Text>{errors}</Callout.Text>
+            </Callout.Root>)}
+        <form className='space-y-3' onSubmit={handleSubmit(onSubmit)}>
+        <TextField.Root placeholder='Title' {...register('title')}/>
+        <Controller
+        name='description'
+        control={control}
+        render={({field}) => <SimpleMDE placeholder='Description' {...field}/>}
+        />
         <Button>Submit New Issue</Button>
+    </form>
     </div>
+    
   )
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import NavBar from './NavBar'
 import { Theme } from "@radix-ui/themes";
+import { ToastContainer } from 'react-toastify';
 
 const inter = Inter({ 
   subsets: ['latin'],
@@ -26,7 +27,9 @@ export default function RootLayout({
       <body className={inter.variable}>
         <Theme>
         <NavBar/>
-        <main className="p-5">{children}</main>
+        <main className="p-5">{children}
+          <ToastContainer />
+        </main>
         </Theme>
       </body>
     </html>
